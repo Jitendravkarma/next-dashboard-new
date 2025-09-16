7661fb5101cb21b54ac58c1eeb68bd5e8b402834
Jitendravkarma <jiturajkarma2020@gmail.com>
2025-09-16 11:25:40 +0530

    added twitter ad script

--- shared/layout-components/public-comp/HeadElement.js
+++ shared/layout-components/public-comp/HeadElement.js
@@ -5,6 +5,25 @@ const HeadElement = () => {
   return (
     <head>
         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(e,t,n,s,u,a){
+                e.twq||(s=e.twq=function(){
+                  s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+                },
+                s.version='1.1',
+                s.queue=[],
+                u=t.createElement(n),
+                u.async=!0,
+                u.src='https://static.ads-twitter.com/uwt.js',
+                a=t.getElementsByTagName(n)[0],
+                a.parentNode.insertBefore(u,a))
+              }(window, document, 'script');
+              twq('config', 'pn69g');
+            `,
+          }}
+        />
+        <script
           id="mcjs"
           dangerouslySetInnerHTML={{
             __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/385185488e96c2387bba7596f/b72597b8ca4e9990309b9354f.js");`,
